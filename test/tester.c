e60a84703346cefb935a91bfd8345be1f0214d6c
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-07-06 10:10:56 +0100

    test: Splits the test fucntions.

--- test/tester.c
+++ test/tester.c
@@ -6,79 +6,72 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 17:58:07 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/07/06 08:18:44 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/07/06 10:08:24 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void	display_nodes(int ac, char **av)
+void	display_node_infos(t_node *node)
 {
-	Node	*head;
-	Node	*curr;
+	ft_printf("	{\n		data: %i,\n", node->data);
+	ft_printf("		next: %p,\n", node->next);
+	ft_printf("		prev: %p,\n", node->prev);
+	ft_printf("		address: %p\n", node);
+	ft_printf("	},\n");
+}
 
-	head = args_to_linked_list(av, ac);
-	if (!head)
-		return ;
+void	free_nodes(t_node *head)
+{
+	t_node	*curr;
+
 	curr = head;
-	ft_printf("[\n");
-	while (!curr->is_last)
+	while (curr)
 	{
-		ft_printf("	{\n		data: %i,\n		is_first: %i,\n		is_last: %i,\n		next: %p,\n		prev: %p,\n		address: %p\n	},\n", curr->data, curr->is_first, curr->is_last, curr->next, curr->prev, curr);
 		curr = curr->next;
 		free(head);
 		head = curr;
 	}
-	ft_printf("	{\n		data: %i,\n		is_first: %i,\n		is_last: %i,\n		next: %p,\n		prev: %p,\n		address: %p\n	}\n", curr->data, curr->is_first, curr->is_last, curr->next, curr->prev, curr);
-	ft_printf("]\n");
-	free(curr);
 }
 
-void	display_swap(int ac, char **av)
+void	display_nodes(t_node *head)
 {
-	Node	*head;
-	Node	*save;
-	Node	*curr;
+	t_node	*curr;
 
-	head = args_to_linked_list(av, ac);
-	if (!head)
-		return ;
 	curr = head;
-	save = head;
-	ft_printf("Before Swap A:\n");
 	ft_printf("[\n");
-	while (!curr->is_last)
+	while (curr->next)
 	{
-		ft_printf("	{\n		data: %i,\n		is_first: %i,\n		is_last: %i,\n		next: %p,\n		prev: %p,\n		address: %p\n	},\n", curr->data, curr->is_first, curr->is_last, curr->next, curr->prev, curr);
+		display_node_infos(curr);
 		curr = curr->next;
 		head = curr;
 	}
-	ft_printf("	{\n		data: %i,\n		is_first: %i,\n		is_last: %i,\n		next: %p,\n		prev: %p,\n		address: %p\n	}\n", curr->data, curr->is_first, curr->is_last, curr->next, curr->prev, curr);
+	display_node_infos(curr);
 	ft_printf("]\n");
-	curr = save;
-	sa(save);
+}
+
+void	display_swap(t_node	*head)
+{
+	ft_printf("Before Swap A:\n");
+	display_nodes(head);
+	sa(head);
 	ft_printf("After Swap A:\n");
-	while (!curr->is_last)
-	{
-		ft_printf("	{\n		data: %i,\n		is_first: %i,\n		is_last: %i,\n		next: %p,\n		prev: %p,\n		address: %p\n	},\n", curr->data, curr->is_first, curr->is_last, curr->next, curr->prev, curr);
-		curr = curr->next;
-		free(save);
-		save = curr;
-	}
-	ft_printf("	{\n		data: %i,\n		is_first: %i,\n		is_last: %i,\n		next: %p,\n		prev: %p,\n		address: %p\n	}\n", curr->data, curr->is_first, curr->is_last, curr->next, curr->prev, curr);
-	ft_printf("]\n");
-	free(curr);
+	display_nodes(head);
 }
 
 int	main(int ac, char **av)
 {
+	t_node	*head;
 
 	if (ac > 1)
 	{
 		if (!args_checker(++av))
 			return (0);
-		display_nodes(ac - 1, av);
-		display_swap(ac - 1, av);
+		head = args_to_linked_list(av, ac - 1);
+		if (!head)
+			return (0);
+		display_nodes(head);
+		display_swap(head);
 	}
 	return (0);
 }
